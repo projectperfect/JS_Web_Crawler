a294b12424bf51b9e65cfc1d94bd7ca39dc22ab3
projectperfect <jprfct@protonmail.com>
2023-01-19 20:58:55 -0800

    jest tests added

---
+++ crawl.js
@@ -0,0 +1,14 @@
+const { builtinModules } = require("module");
+
+function normalizeURL(url) {
+  const urlObj = new URL(url);
+  let fullPath = `${urlObj.host}${urlObj.pathname}`;
+  if (fullPath.length > 0 && fullPath.slice(-1) === "/") {
+    fullPath = fullPath.slice(0, -1);
+  }
+  return fullPath;
+}
+
+modules.exports = {
+  normalizeURL,
+};
